fcdd9d4f3b0a4605c1393788313a6ae049f7e398
Laptop Alnino <alnino007@gmail.com>
2024-10-12 14:51:29 +0700

    wip finished event

--- app/src/main/java/com/example/dicodingevents/ui/upcomingevents/UpcomingEventsFragment.kt
+++ app/src/main/java/com/example/dicodingevents/ui/upcomingevents/UpcomingEventsFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dicodingevents.EventAdapter
+import com.example.dicodingevents.data.response.ListEventsItem
 import com.example.dicodingevents.databinding.FragmentUpcomingEventsBinding
 
 class UpcomingEventsFragment : Fragment() {
@@ -22,17 +24,28 @@ class UpcomingEventsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentUpcomingEventsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        upcomingEventsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val layoutManager = LinearLayoutManager(this@UpcomingEventsFragment.context)
+        binding.rvUpcomingEvents.layoutManager = layoutManager
+
+        upcomingEventsViewModel.listEventsItem.observe(viewLifecycleOwner){
+            setEventsData(it)
         }
-        return root
+
+        return binding.root
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    private fun setEventsData (events: List<ListEventsItem?>?){
+        val adapter = EventAdapter(events)
+        binding.rvUpcomingEvents.adapter = adapter
+    }
+
+
 }

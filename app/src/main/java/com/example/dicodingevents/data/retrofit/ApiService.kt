d32a23808d94b012efbe37224ed0e8c25094481b
AlninoDP <alninodp@gmail.com>
2024-10-12 09:42:54 +0700

    wip: get data from api

---
+++ app/src/main/java/com/example/dicodingevents/data/retrofit/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.dicodingevents.data.retrofit
+
+import com.example.dicodingevents.data.response.ResponseEvents
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class ApiService {
+//    @GET("events")
+//    fun getEvents(
+//        @Query("active") active: Int?,
+//        @Query("q") q: String? ,
+//        @Query("limit") limit: Int?
+//    ): Call<ResponseEvents>
+}
